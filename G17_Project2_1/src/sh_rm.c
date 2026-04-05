bb8bf74a623b23d829c2a3a2ada92d18219bbf13
Sanjib Behera <178703817+sanjib2006@users.noreply.github.com>
2026-04-06 00:25:09 +0530

    unlink for files and rmdir for directories

--- G17_Project2_1/src/sh_rm.c
+++ G17_Project2_1/src/sh_rm.c
@@ -15,16 +15,37 @@ int main(int argc, char *argv[])
         perror("Error finding target");
         return 1;
     }
+
+    printf("You are going to delete: %s\n", argv[1]);
+
     if (S_ISDIR(file_info.st_mode))
     {
         printf("Directory\n");
+        if (rmdir(argv[1]) == 0)
+        {
+            printf("sh_rm: Directory %s deleted successfully.\n", argv[1]);
+            return 0;
+        }
+        else
+        {
+            perror("Could not delete directory");
+            return 1;
+        }
     }
     else
     {
         printf("File\n");
+        if (unlink(argv[0]) == 0)
+        {
+            printf("sh_rm: File %s deleted successfully.\n", argv[1]);
+            return 0;
+        }
+        else
+        {
+            perror("Could not delete file");
+            return 1;
+        }
     }
-
-    printf("You are going to delete: %s\n", argv[1]);
 
     return 0;
 }
