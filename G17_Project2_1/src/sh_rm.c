49874b20ba931546bb6d8a164f614ac6c1a8e698
Sanjib Behera <178703817+sanjib2006@users.noreply.github.com>
2026-04-10 23:28:39 +0530

    refactor sh_rm to handle multiple files

--- G17_Project2_1/src/sh_rm.c
+++ G17_Project2_1/src/sh_rm.c
@@ -6,45 +6,45 @@ int main(int argc, char *argv[])
 {
     struct stat file_info;
 
-    if (argc != 2)
+    if (argc < 2)
     {
-        printf("Usage: sh_rm <filename>\n");
-        return 1;
-    }
-    if (stat(argv[1], &file_info) != 0)
-    {
-        perror("Error finding target");
+        printf("Usage: sh_rm <file1> <file2> ...\n");
         return 1;
     }
 
-    printf("You are going to delete: %s\n", argv[1]);
+    for (int i = 1; i < argc; i++)
+    {
+        if (stat(argv[i], &file_info) != 0)
+        {
+            perror("Error finding target");
+            continue; // move to next file
+        }
 
-    if (S_ISDIR(file_info.st_mode))
-    {
-        printf("Directory\n");
-        if (rmdir(argv[1]) == 0)
+        printf("You are going to delete: %s\n", argv[i]);
+
+        if (S_ISDIR(file_info.st_mode))
         {
-            printf("sh_rm: Directory %s deleted successfully.\n", argv[1]);
-            return 0;
+            printf("Directory\n");
+            if (rmdir(argv[i]) == 0)
+            {
+                printf("sh_rm: Directory %s deleted successfully.\n", argv[i]);
+            }
+            else
+            {
+                perror("Could not delete directory");
+            }
         }
         else
         {
-            perror("Could not delete directory");
-            return 1;
-        }
-    }
-    else
-    {
-        printf("File\n");
-        if (unlink(argv[1]) == 0)
-        {
-            printf("sh_rm: File %s deleted successfully.\n", argv[1]);
-            return 0;
-        }
-        else
-        {
-            perror("Could not delete file");
-            return 1;
+            printf("File\n");
+            if (unlink(argv[i]) == 0)
+            {
+                printf("sh_rm: File %s deleted successfully.\n", argv[i]);
+            }
+            else
+            {
+                perror("Could not delete file");
+            }
         }
     }
 
